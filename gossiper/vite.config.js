98e128f3ad46f9156fd8595b5f408c8cb743e8aa
Andrey Konovalenko <andreinew@mail.ru>
2023-08-04 16:59:20 +0300

    chore: added main-layout.hbs

--- gossiper/vite.config.js
+++ gossiper/vite.config.js
@@ -1,10 +1,23 @@
+import { defineConfig } from "vite";
 import { resolve } from "path";
 import handlebars from "vite-plugin-handlebars";
 
-export default {
+export default defineConfig({
   plugins: [
     handlebars({
-      partialDirectory: resolve(__dirname, "partials"),
+      partialDirectory: resolve(__dirname, "src/partials"),
+      context: {
+        title: "My Chat",
+      },
     }),
   ],
-};
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+});
+
+console.log(co);

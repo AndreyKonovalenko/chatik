94379457e30b22c421ba8e3ea7491b9e5869ed15
AndreyKonovalenko <andreinew@mail.ru>
2023-08-11 00:42:02 +0300

    chore: change app name add figma prototype link

--- gossiper/server.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const PORT = 3000;
-const app = express();
-
-app.use(express.static('./dist'));
-app.listen(PORT, () => {
-  console.log(`Мой текст в логе после запуска ${PORT}!`);
-});

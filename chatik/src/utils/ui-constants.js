ceb78303ed83501f9b363862127512df022995d9
AndreyKonovalenko <andreinew@mail.ru>
2023-08-14 17:02:34 +0300

    chore: finished side-bar added icon component

--- chatik/src/utils/ui-constants.js
+++ chatik/src/utils/ui-constants.js
@@ -3,6 +3,12 @@ const uiConstants = {
   CREATE_ACCOUNT: 'Create account',
   WRONG_PASSWORD: 'wrong password',
   WRONG_LOGIN: 'wrong login',
+  palette: {
+    primary: '#bb86fc',
+    light: '#c89efc',
+    dark: '#825db0',
+    onPrimary: '#ffffff',
+  },
 };
 
 export default uiConstants;

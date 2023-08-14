5a5ba21a209256942cd6e4e7091fcaaeee13e0bc
AndreyKonovalenko <andreinew@mail.ru>
2023-08-14 14:38:21 +0300

    chore: complited login form added side-bar

--- chatik/src/utils/ui-constants.js
+++ chatik/src/utils/ui-constants.js
@@ -1,5 +1,8 @@
 const uiConstants = {
-  LOGIN: 'LOGIN',
+  LOGIN: 'Login',
+  CREATE_ACCOUNT: 'Create account',
+  WRONG_PASSWORD: 'wrong password',
+  WRONG_LOGIN: 'wrong login',
 };
 
 export default uiConstants;

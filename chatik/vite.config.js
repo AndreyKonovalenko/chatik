45d2a074aa6d1d1a79515f776a2bf13b1da2bbaf
AndreyKonovalenko <andreinew@mail.ru>
2023-08-25 23:31:04 +0300

    chore: added delete chat and delete user modals

--- chatik/vite.config.js
+++ chatik/vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import handlebars from "vite-plugin-handlebars";
-import uiConstants from "./src/utils/ui-constants";
-import _mock_users from "./src/__mocks__/_mock_users";
-import _mock_chats from "./src/__mocks__/_mock_chats";
-import dateFormatter from "./src/helpers/date-formater";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
+import uiConstants from './src/utils/ui-constants';
+import _mock_users from './src/__mocks__/_mock_users';
+import _mock_chats from './src/__mocks__/_mock_chats';
+import dateFormatter from './src/helpers/date-formater';
 
 export default defineConfig({
   plugins: [
     handlebars({
-      partialDirectory: resolve(__dirname, "src/partials"),
+      partialDirectory: resolve(__dirname, 'src/partials'),
       context: {
-        title: "Chatik",
+        title: 'Chatik',
         uiConstants: uiConstants,
         _mock_users: _mock_users,
         _mock_chats: _mock_chats,
@@ -24,9 +24,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        index: resolve(__dirname, "index.html"),
-        login: resolve(__dirname, "src/pages/login/login.html"),
-        register: resolve(__dirname, "src/pages/register/register.html"),
+        index: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'src/pages/login/login.html'),
+        register: resolve(__dirname, 'src/pages/register/register.html'),
       },
     },
   },

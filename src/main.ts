2631c59317979a52a217b8fbc11452ec9388566b
Andrey Konovalenko <andreinew@mail.ru>
2023-09-08 17:12:54 +0300

    chore: ts setup

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,61 @@
 import Handlebars from 'handlebars';
-import * as Components from './components';
-import * as Pages from './pages';
-import { registerComponent } from './core/resgiterComponent';
+// import * as Components from './components';
+// import * as Pages from './pages';
+//import { registerComponent } from './core/resgiterComponent';
+
+import { Layout } from './components/layout';
+
+console.log(Layout)
+// const pages = {
+//   'login': Layout,
+//   'list': Layout,
+// };
 
 
-const pages = {
-  'login': Pages.LoginPage,
-  'list': Pages.ListPage,
-};
+Handlebars.registerPartial('Layout', Layout);
+// Handlebars.registerPartial('ListCat', Components.ListCat);
+// Handlebars.registerPartial('CatCard', Components.CatCard);
+
+// registerComponent('Button', Components.Button);
+// registerComponent('InputField', Components.InputField);
+// registerComponent('Input', Components.Input);
+// registerComponent('ErrorLine', Components.ErrorLine);
 
 
-Handlebars.registerPartial('FormAuth', Components.FormAuth);
-Handlebars.registerPartial('ListCat', Components.ListCat);
-Handlebars.registerPartial('CatCard', Components.CatCard);
+// function navigate(page: string) {
+//   const app = document.getElementById('app');
 
-registerComponent('Button', Components.Button);
-registerComponent('InputField', Components.InputField);
-registerComponent('Input', Components.Input);
-registerComponent('ErrorLine', Components.ErrorLine);
+//   if(page === 'list') {
+//     const container = document.getElementById('app')!;
+//     container.innerHTML = Handlebars.compile(Layout)({});
+//     return;
+//   }
+
+//   //@ts-ignore
+//   const Component = pages[page]
+//   const component = new Component();
+//   app?.append(component.getContent()!);
+
+// }
+
+// document.addEventListener('DOMContentLoaded', () => navigate('login'));
+
+// document.addEventListener('click', e => {
+//   //@ts-ignore
+//   const page = e.target.getAttribute('page');
+//   if (page) {
+//     navigate(page);
+
+//     e.preventDefault();
+//     e.stopImmediatePropagation();
+//   }
+// });
 
 
-function navigate(page: string) {
-  const app = document.getElementById('app');
-
-  if(page === 'list') {
-    const container = document.getElementById('app')!;
-    container.innerHTML = Handlebars.compile(pages[page])({});
-    return;
-  }
-
-  //@ts-ignore
-  const Component = pages[page]
-  const component = new Component();
-  app?.append(component.getContent()!);
-
+const render = () => {
+  const container = document.getElementById('app')!;
+  console.log('hello')
+  container.innerHTML = Handlebars.compile(Layout)({});
 }
 
-document.addEventListener('DOMContentLoaded', () => navigate('login'));
-
-document.addEventListener('click', e => {
-  //@ts-ignore
-  const page = e.target.getAttribute('page');
-  if (page) {
-    navigate(page);
-
-    e.preventDefault();
-    e.stopImmediatePropagation();
-  }
-});
+document.addEventListener('DOMContentLoaded', () => render());

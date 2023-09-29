220cb8d253619423d9cc0f6a169db8cecb8cfa18
Andrey Konovalenko <andreinew@mail.ru>
2023-09-29 17:26:19 +0300

    chrore: fix types

--- src/core/EventBus.ts
+++ src/core/EventBus.ts
@@ -1,36 +1,33 @@
-type Callback = (...args: object[]) => void;
+export type Listener<T extends unknown[] = any[]> = (...args: T) => void;
 
-export class EventBus {
-  private listeners: { [event: string]: Callback[] };
+export  class EventBus<E extends string = string, M extends { [K in E]: unknown[] } = Record<E, any[]>> {
+  private listeners: { [key in E]?: Listener<M[E]>[] } = {};
 
-  constructor() {
-    this.listeners = {};
-  }
-
-  on(event: string, callback: Callback): void {
+  on(event: E, callback: Listener<M[E]>) {
     if (!this.listeners[event]) {
       this.listeners[event] = [];
     }
 
-    this.listeners[event].push(callback);
+    this.listeners[event]!.push(callback);
   }
 
-  off(event: string, callback: Callback): void {
+  off(event: E, callback: Listener<M[E]>) {
     if (!this.listeners[event]) {
       throw new Error(`Нет события: ${event}`);
     }
 
-    this.listeners[event] = this.listeners[event].filter(
-      (listener) => listener !== callback
+    this.listeners[event] = this.listeners[event]!.filter(
+      listener => listener !== callback,
     );
   }
 
-  emit(event: string, ...args: object[]): void {
+  emit(event: E, ...args: M[E]) {
     if (!this.listeners[event]) {
-      throw new Error(`Нет события: ${event}`);
+      return;
+      // throw new Error(`Нет события: ${event}`);
     }
 
-    this.listeners[event].forEach((listener) => {
+    this.listeners[event]!.forEach(function (listener) {
       listener(...args);
     });
   }

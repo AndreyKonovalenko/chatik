cdcd6d39b25cbefcc1efcdcedfc3a7834926d0e6
AndreyKonovalenko <andreinew@mail.ru>
2023-09-29 22:57:50 +0300

    chore: chaned ts config

--- src/core/Block.ts
+++ src/core/Block.ts
@@ -11,7 +11,7 @@ class Block<Props extends Record<string, unknown>> {
   };
 
   public id: string = nanoid(6);
-  protected props  = {} as Props;
+  protected props = {} as Props;
   protected refs: Record<string, Block<Props> | Block<Props>[]> = {};
   private eventBus: () => EventBus;
   private _element: HTMLElement | null = null;
@@ -19,21 +19,30 @@ class Block<Props extends Record<string, unknown>> {
 
   constructor(propsWithChildren = {}) {
     const eventBus = new EventBus();
-    const { props, children } = this._getChildrenAndProps(propsWithChildren as Props);
+    const { props, children } = this._getChildrenAndProps(
+      propsWithChildren as Props
+    );
     this.props = this._makePropsProxy(props, this);
     this.eventBus = () => eventBus;
     this._registerEvents(eventBus);
     eventBus.emit(Block.EVENTS.INIT);
-    this.children = children
+    this.children = children;
   }
 
-  private _getChildrenAndProps(childrenAndProps: Props) : {props: Props, children: Record<string, Block<Props> | Block<Props>[]>} {
+  private _getChildrenAndProps(childrenAndProps: Props): {
+    props: Props;
+    children: Record<string, Block<Props> | Block<Props>[]>;
+  } {
     const props: Record<string, unknown> = {};
     const children: Record<string, Block<Props> | Block<Props>[]> = {};
 
     for (const [key, value] of Object.entries(childrenAndProps)) {
-      if ((value instanceof Block || Array.isArray(value))
-      && ((value as []).every((item : HTMLElement | HTMLInputElement) => item instanceof Block))) {
+      if (
+        (value instanceof Block || Array.isArray(value)) &&
+        (value as []).every(
+          (item: HTMLElement | HTMLInputElement) => item instanceof Block
+        )
+      ) {
         children[key] = value;
       } else {
         props[key] = value;
@@ -137,7 +146,10 @@ class Block<Props extends Record<string, unknown>> {
     const contextAndStubs = {
       ...context,
       __refs: this.refs,
-      __children: [] as Array<{component: unknown, embed(node: DocumentFragment):void}>,
+      __children: [] as Array<{
+        component: unknown;
+        embed(node: DocumentFragment): void;
+      }>,
     };
 
     const html = Handlebars.compile(template)(contextAndStubs);
